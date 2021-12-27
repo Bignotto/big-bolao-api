4bbd53a0e530b5be097088009fa350e0a30f26cd
Thiago Bignotto <bignotto@gmail.com>
2021-12-27 11:02:47 -0300

    implemented authenticate user use case

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,47 @@
+import { compare } from 'bcryptjs';
+import { AppError } from '../../../../shared/errors/AppError';
+import { IUserRepository } from '../../repositories/IUserRepository';
+
+import { sign } from 'jsonwebtoken';
+import auth from '../../../../config/auth';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+  // refresh_token: string;
+}
+
+class AuthenticateUserUseCase {
+  constructor(private usersRepository: IUserRepository) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) throw new AppError('Wrong credentials', 401);
+
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) throw new AppError('Wrong credentials', 401);
+
+    const token = sign({}, auth.secret, {
+      subject: user.id,
+      expiresIn: auth.expires_in,
+    });
+
+    return {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+  }
+}
+
+export { AuthenticateUserUseCase };
