ce60dc903bfe7bcb313e1082f824b5369bbc8069
Thiago Bignotto <bignotto@gmail.com>
2021-12-28 07:47:21 -0300

    fixed: hide password when returning user information

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -2,8 +2,10 @@ import { User } from '../../entities/User';
 import { AppError } from '@shared/errors/AppError';
 import { ICreateUserDTO } from '../../dtos/CreateUserDTO';
 import { IUserRepository } from '../../repositories/IUserRepository';
+import { UserMap } from '@modules/accounts/mapper/UserMap';
 
 import { hash } from 'bcryptjs';
+import { IUserResponseDTO } from '@modules/accounts/dtos/UserResponseDTO';
 
 class CreateUserUseCase {
   constructor(private usersRepository: IUserRepository) {}
@@ -15,7 +17,7 @@ class CreateUserUseCase {
     favTeam,
     profile,
     avatar,
-  }: ICreateUserDTO): Promise<User> {
+  }: ICreateUserDTO): Promise<IUserResponseDTO> {
     if (!email || !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
       throw new AppError('E-Mail address is invalid.', 400);
 
@@ -35,7 +37,8 @@ class CreateUserUseCase {
       profile,
       avatar,
     });
-    return newUser;
+    const user = UserMap.toDto(newUser);
+    return user;
   }
 }
 
