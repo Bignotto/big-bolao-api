30b981923c51d67ca7a7e254963562e0e2f16983
Thiago Bignotto <bignotto@gmail.com>
2022-01-02 20:21:11 -0300

    implemented tests

--- src/modules/groups/useCases/createGroup/CreateGroupUseCase.spec.ts
+++ src/modules/groups/useCases/createGroup/CreateGroupUseCase.spec.ts
@@ -2,6 +2,7 @@ import { User } from '@modules/accounts/entities/User';
 import { UserInMemoryRepository } from '@modules/accounts/repositories/inMemory/UserInMemoryRepository';
 import { CreateUserUseCase } from '@modules/accounts/useCases/createUser/CreateUserUseCase';
 import { GroupInMemoryRepository } from '@modules/groups/repositories/inMemory/GroupInMemoryRepository';
+import { AppError } from '@shared/errors/AppError';
 import { CreateGroupUseCase } from './CreateGroupUseCase';
 
 let createGroupUseCase: CreateGroupUseCase;
@@ -30,13 +31,48 @@ describe('Create Group Use Case', () => {
       password: '123456',
     });
 
+    const createdUser = await createUserUseCase.execute(user);
+    user.id = createdUser.id;
+
     const createdGroup = await createGroupUseCase.execute({
       description: 'Test Group',
-      owner_id: user.id,
+      owner_id: createdUser.id,
       password: '123',
       users: [user],
     });
 
     expect(createdGroup).toHaveProperty('id');
+  });
+
+  it('should not be able to create a group when invalid user id is passed', async () => {
+    await expect(() =>
+      createGroupUseCase.execute({
+        description: 'Test Group',
+        owner_id: 'invalid id',
+        password: '123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it(`group owner should be in the group's users array`, async () => {
+    const user = new User();
+    Object.assign(user, {
+      name: 'Test Dude',
+      email: 'dude@test.com',
+      password: '123456',
+    });
+
+    const createdUser = await createUserUseCase.execute(user);
+    user.id = createdUser.id;
+
+    const createdGroup = await createGroupUseCase.execute({
+      description: 'Test Group',
+      owner_id: createdUser.id,
+      password: '123',
+      users: [user],
+    });
+
+    expect(createdGroup.users.length).toBe(1);
+    expect(createdGroup.owner_id).toEqual(createdGroup.users[0].id);
   });
 });
--- src/modules/groups/useCases/createGroup/CreateGroupUseCase.ts
+++ src/modules/groups/useCases/createGroup/CreateGroupUseCase.ts
@@ -5,6 +5,7 @@ import { IGroupRepository } from '../../repositories/IGroupRepository';
 import { Group } from '@modules/groups/entities/Group';
 import { ICreateGroupDTO } from '@modules/groups/dtos/ICreateGroupDTO';
 import { IUserRepository } from '@modules/accounts/repositories/IUserRepository';
+import { AppError } from '@shared/errors/AppError';
 
 @injectable()
 class CreateGroupUseCase {
@@ -20,12 +21,15 @@ class CreateGroupUseCase {
     users,
   }: ICreateGroupDTO): Promise<Group> {
     const owner = await this.userRepository.findById(owner_id);
+    if (!owner) throw new AppError('Invalid user', 400);
+
+    if (!users) users.push(owner);
 
     const group = await this.groupRepository.create({
       description,
       owner_id,
       password,
-      users: [owner],
+      users,
     });
 
     return group;
