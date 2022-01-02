d95347aca726fa976c0049b94f44642c069e5045
Thiago Bignotto <bignotto@gmail.com>
2022-01-02 07:57:00 -0300

    implemented create team table migration

--- src/modules/matches/entities/Team.ts
+++ src/modules/matches/entities/Team.ts
@@ -1,8 +1,8 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('teams')
 class Team {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn('increment')
   id: number;
 
   @Column()
