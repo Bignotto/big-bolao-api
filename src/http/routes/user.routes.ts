22e990c13007d3b0f378f3055b0317878fcd278c
Thiago Bignotto <bignotto@gmail.com>
2025-03-13 06:20:20 -0300

    :sparkles: feat: add JWT authentication middleware to user routes
    - Add verifyJwt middleware to protect all user routes
    - Import verifyJwt middleware from middlewares directory
    - Add Authorization header in API test file

--- src/http/routes/user.routes.ts
+++ src/http/routes/user.routes.ts
@@ -2,8 +2,11 @@ import { FastifyInstance } from 'fastify';
 import { CreateUserController } from '../controllers/user/createUserController';
 import { GetUserInfoController } from '../controllers/user/getUserInfoController';
 import { UpdateUserController } from '../controllers/user/updateUserController';
+import { verifyJwt } from '../middlewares/verifyJWT';
 
 export async function UserRoutes(app: FastifyInstance) {
+  app.addHook('onRequest', verifyJwt);
+
   app.post('/users', CreateUserController);
   app.put('/users/:userId', UpdateUserController);
   app.get('/users/:userId', GetUserInfoController);
