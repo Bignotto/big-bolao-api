92e074ea2dd09e202dd6be6a61ae6ee001e5ba4b
Thiago Bignotto <bignotto@gmail.com>
2025-03-24 06:44:14 -0300

    :hammer: feat: user pool list
    - add get users pools list
    - add get users pools controller
    - add get users pools unit tests

---
+++ src/http/controllers/pools/getUserPools.ts
@@ -0,0 +1,24 @@
+import { ResourceNotFoundError } from '@/global/errors/ResourceNotFoundError';
+import { makeGetUserPoolsUseCase } from '@/useCases/pools/factories/makeGetUserPoolsUseCase';
+import { FastifyReply, FastifyRequest } from 'fastify';
+
+export async function getUserPools(request: FastifyRequest, reply: FastifyReply) {
+  try {
+    const userId = request.user.sub;
+
+    const getUserPoolsUseCase = makeGetUserPoolsUseCase();
+    const { pools } = await getUserPoolsUseCase.execute({
+      userId,
+    });
+
+    return reply.status(200).send({
+      pools,
+    });
+  } catch (error) {
+    if (error instanceof ResourceNotFoundError) {
+      return reply.status(404).send({ message: error.message });
+    }
+
+    throw error;
+  }
+}
---
+++ src/useCases/pools/factories/makeGetUserPoolsUseCase.ts
@@ -0,0 +1,11 @@
+import { PrismaPoolsRepository } from '@/repositories/pools/PrismaPoolsRepository';
+import { PrismaUsersRepository } from '@/repositories/users/PrismaUsersRepository';
+import { GetUserPoolsUseCase } from '../getUserPoolsUseCase';
+
+export function makeGetUserPoolsUseCase() {
+  const poolsRepository = new PrismaPoolsRepository();
+  const usersRepository = new PrismaUsersRepository();
+  const useCase = new GetUserPoolsUseCase(poolsRepository, usersRepository);
+
+  return useCase;
+}
