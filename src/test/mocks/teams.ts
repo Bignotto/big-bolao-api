e090016740c4bfe6bace2067d2792942db90202e
Thiago Bignotto <bignotto@gmail.com>
2025-04-05 11:41:20 -0300

    :hammer: feat: refactor createTeam and createTournament functions to accept repository as a parameter

--- src/test/mocks/teams.ts
+++ src/test/mocks/teams.ts
@@ -1,16 +1,20 @@
+import { ITeamsRepository } from '@/repositories/teams/ITeamsRepository';
 import { fakerPT_BR as faker } from '@faker-js/faker';
 import { Team } from '@prisma/client';
 
-export async function createTeam(data: {
-  name?: string;
-  countryCode?: string;
-  flagUrl?: string;
-}): Promise<Team> {
+export async function createTeam(
+  repository: ITeamsRepository,
+  data: {
+    name?: string;
+    countryCode?: string;
+    flagUrl?: string;
+  }
+): Promise<Team> {
   const randomTeamNumber = Math.floor(Math.random() * 100);
-  return {
-    id: randomTeamNumber,
+  const team = await repository.create({
     name: data.name ?? `Team ${randomTeamNumber}`,
     countryCode: data.countryCode ?? faker.location.countryCode(),
     flagUrl: data.flagUrl ?? 'https://example.com/flag.png',
-  } as Team;
+  });
+  return team;
 }
